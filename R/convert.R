1e606b9823288c260d59c107649cbb570e6444fe
Jeroen <jeroenooms@gmail.com>
2018-08-27 11:33:46 +0200

    Documentation and such

--- R/convert.R
+++ R/convert.R
@@ -4,6 +4,9 @@
 #'
 #' @export
 #' @useDynLib av R_convert
+#' @param input vector with image or video files
+#' @param output filename of the output file. Container type is inferred from
+#' the file extension which should typically be `.mkv` or `.mp4`.
 av_convert <- function(input, output){
   input <- normalizePath(input, mustWork = TRUE)
   output <- normalizePath(output, mustWork = FALSE)
--- R/formats.R
+++ R/formats.R
@@ -4,11 +4,13 @@
 #' 
 #' 
 #' @export
+#' @rdname formats
 list_encoders <- function(){
   list_codecs(TRUE)
 }
 
 #' @export
+#' @rdname formats
 list_decoders <- function(){
   list_codecs(FALSE)
 }
@@ -18,5 +20,8 @@ list_codecs <- function(is_encoder){
   res <- as.list(.Call(R_list_codecs))
   names(res) <- c("type", "encoder", "name", "description")
   df <- data.frame(res, stringsAsFactors = FALSE)
-  df[df$encoder == is_encoder, c("type", "name", "description")]
+  df <- df[df$encoder == is_encoder, c("type", "name", "description")]
+  df <- df[order(df$name),]
+  row.names(df) <- NULL
+  df
 }
