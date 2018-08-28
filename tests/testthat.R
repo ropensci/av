39df0f5b8961dcc9697b13adbf01aa0103fb5a49
Jeroen <jeroenooms@gmail.com>
2018-08-28 12:45:56 +0000

    Revert "Fix unit test for Windows"
    This reverts commit ba4d3d97077efdf955cc0a9462b6c5d704cd8b7f.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,7 @@
 library(testthat)
 library(av)
 
+download.file("http://www.google.com", tempfile(), quiet = TRUE)
 reporter <- if (ps::ps_is_supported()) {
   ps::CleanupReporter(testthat::SummaryReporter)$new(proc_cleanup =  FALSE,  proc_fail = FALSE)
 } else {
--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -8,8 +8,8 @@ test_that("Set log level", {
 test_that("mp4 to mkv", {
   input <- file.path(tempdir(), 'input.mp4')
   output <- file.path(tempdir(), 'output.mkv')
-  curl::curl_download('http://techslides.com/demos/sample-videos/small.mp4', 
-                input, quiet = TRUE)
+  download.file('http://techslides.com/demos/sample-videos/small.mp4', 
+                input, mode = 'wb', quiet = TRUE)
   av_convert(input, output)
   expect_true(file.exists(output))
 })
