9f73fb22f1f136863afe9e92def7141b0928f498
Jeroen <jeroenooms@gmail.com>
2018-08-31 22:53:33 +0200

    Remove hack from unit test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,6 @@
 library(testthat)
 library(av)
 
-download.file("http://www.google.com", tempfile(), quiet = TRUE)
 reporter <- if (ps::ps_is_supported()) {
   ps::CleanupReporter(testthat::SummaryReporter)$new(proc_cleanup =  FALSE,  proc_fail = FALSE)
 } else {
