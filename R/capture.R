0eec523ac62fdec867a7940b3d9b4a3fb4df3054
Jeroen <jeroenooms@gmail.com>
2018-09-11 17:43:23 +0200

    Rename some stuff

--- R/capture.R
+++ R/capture.R
@@ -1,4 +1,4 @@
-#' Save Video from Graphics Device
+#' Record Video from Graphics Device
 #'
 #' Runs the expression and captures all plots into a video.
 #'
@@ -25,10 +25,10 @@
 #'
 #' # Play 1 plot per sec, and use an interpolation filter to convert into 10 fps
 #' video_file <- file.path(tempdir(), 'output.mp4')
-#' av_save_video(makeplot(), video_file, 1280, 720, res = 144, filter = 'framerate=fps=10')
+#' av_capture_graphics(makeplot(), video_file, 1280, 720, res = 144, filter = 'framerate=fps=10')
 #' av::av_video_info(video_file)
 #' utils::browseURL(video_file)}
-av_save_video <- function(expr, output = 'output.mp4', width = 720, height = 480, framerate = 1,
+av_capture_graphics <- function(expr, output = 'output.mp4', width = 720, height = 480, framerate = 1,
                        filter = "null", verbose = TRUE, ...){
   imgdir <- tempfile('tmppng')
   dir.create(imgdir)
