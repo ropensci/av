485721b9b9d162450a2bfb982ef2ffd62442e47d
Jeroen <jeroenooms@gmail.com>
2018-09-18 14:10:46 +0200

    Add pixel format to info

--- src/info.c
+++ src/info.c
@@ -21,12 +21,13 @@ static void bail_if(int ret, const char * what){
 }
 
 static SEXP get_video_info(AVFormatContext *ifmt_ctx){
-  SEXP names = PROTECT(Rf_allocVector(STRSXP, 5));
+  SEXP names = PROTECT(Rf_allocVector(STRSXP, 6));
   SET_STRING_ELT(names, 0, Rf_mkChar("width"));
   SET_STRING_ELT(names, 1, Rf_mkChar("height"));
   SET_STRING_ELT(names, 2, Rf_mkChar("codec"));
   SET_STRING_ELT(names, 3, Rf_mkChar("frames"));
   SET_STRING_ELT(names, 4, Rf_mkChar("framerate"));
+  SET_STRING_ELT(names, 5, Rf_mkChar("format"));
   for (int i = 0; i < ifmt_ctx->nb_streams; i++) {
     AVStream *stream = ifmt_ctx->streams[i];
     if(stream->codecpar->codec_type != AVMEDIA_TYPE_VIDEO)
@@ -38,9 +39,10 @@ static SEXP get_video_info(AVFormatContext *ifmt_ctx){
     SEXP streamdata = PROTECT(Rf_allocVector(VECSXP, Rf_length(names)));
     SET_VECTOR_ELT(streamdata, 0, Rf_ScalarReal(stream->codecpar->width));
     SET_VECTOR_ELT(streamdata, 1, Rf_ScalarReal(stream->codecpar->height));
-    SET_VECTOR_ELT(streamdata, 2, Rf_mkString(codec->name));
+    SET_VECTOR_ELT(streamdata, 2, safe_string(codec->name));
     SET_VECTOR_ELT(streamdata, 3, Rf_ScalarReal(stream->nb_frames ? stream->nb_frames : NA_REAL));
     SET_VECTOR_ELT(streamdata, 4, Rf_ScalarReal((double)framerate.num / framerate.den));
+    SET_VECTOR_ELT(streamdata, 5, safe_string(av_get_pix_fmt_name((enum AVPixelFormat) stream->codecpar->format)));
     Rf_setAttrib(streamdata, R_NamesSymbol, names);
     UNPROTECT(2);
     return streamdata;
