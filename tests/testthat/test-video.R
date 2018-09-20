2f996bef663b0043bb5bbefc42bc20827e5600b7
Jeroen <jeroenooms@gmail.com>
2018-09-20 16:59:56 +0200

    Add unit tests for audio stream

--- tests/testthat/test-video.R
+++ tests/testthat/test-video.R
@@ -5,14 +5,14 @@ test_that("Set log level", {
   expect_equal(av_log_level(), 16)
 })
 
+q <- 1
+n <- 50
+framerate <- 10
+width <- 640 * q
+height <- 480 * q
 png_files <- NULL
 
 test_that("convert images into video formats", {
-  q <- 1
-  n <- 50
-  framerate <- 10
-  width <- 640 * q
-  height <- 480 * q
   png_path <- file.path(tempdir(), "frame%03d.png")
   png(png_path, width = width, height = height, res = 72 * q)
   par(ask = FALSE)
@@ -21,8 +21,7 @@ test_that("convert images into video formats", {
   }
   dev.off()
   png_files <<- sprintf(png_path, 1:n)
-  video_types <- c("mkv", "mp4", "mov", "flv", "gif")
-  for(ext in video_types){
+  for(ext in c("mkv", "mp4", "mov", "flv", "gif")){
     filename <- paste0("test.", ext)
     av::av_encode_video(png_files, filename, framerate = framerate, verbose = FALSE)
     expect_true(file.exists(filename))
@@ -36,6 +35,29 @@ test_that("convert images into video formats", {
     # Gif doesn't have video metadata (duration)
     if(ext != 'gif')
       expect_equal(info$duration, (n-1) / framerate)
+  }
+})
+
+test_that("audio sampling works", {
+  wonderland <- system.file('samples/Synapsis-Wonderland.mp3', package='av', mustWork = TRUE)
+  for(ext in c("mkv", "mp4", "mov", "flv")){
+    filename <- paste0("test.", ext)
+    av::av_encode_video(png_files, filename, framerate = framerate, verbose = FALSE, audio = wonderland)
+    expect_true(file.exists(filename))
+    info <- av_video_info(filename)
+    unlink(filename)
+    expect_equal(info$video$width, width)
+    expect_equal(info$video$height, height)
+    expect_equal(info$video$codec, switch(ext, gif='gif', flv='flv', 'h264'))
+    expect_equal(info$video$framerate, framerate)
+
+    expect_equal(info$audio$channels, 2)
+    expect_equal(info$audio$sample_rate, 44100)
+    expect_equal(substring(info$audio$codec, 1, 3), switch(ext, mkv='ac3', flv='mp3', 'aac'))
+
+    # Audio stream may slightly alter the duration, 1 sec margin
+    expect_gte(info$duration, (n-1) / framerate)
+    expect_lt(info$duration - 1, (n-1) / framerate)
   }
 })
 
