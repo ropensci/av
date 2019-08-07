50b4773c16eaa98f68e2d32447b79c635d805491
Jeroen Ooms <jeroenooms@gmail.com>
2019-08-08 01:53:19 +0200

    Safety

--- src/video.c
+++ src/video.c
@@ -513,7 +513,7 @@ int read_from_input(const char *filename, output_container *output){
       if(feed_to_filter(picture, output))
         return 1;
     } while(ret == 0);
-    break; // one video stream per file is enough
+    return 0; // one video stream per input file is enough
   }
   Rf_error("Input data does not contain suitable video stream");
 }
