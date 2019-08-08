937b73176973013456e268c8658c3a0548edfd25
Jeroen Ooms <jeroenooms@gmail.com>
2019-08-08 23:33:18 +0200

    Split images

---
+++ R/images.R
@@ -0,0 +1,18 @@
+#' Split Video into PNG
+#'
+#' Convert a video file in a set of PNG images.
+#'
+#' @export
+#' @param video an input video
+#' @param destdir directory where to save the png files
+#' @param fps sample rate of images. Use `NULL` to get all images.
+av_video_images <- function(video, destdir = tempfile(), fps = 1){
+  stopifnot(length(video) == 1)
+  vfilter <- if(length(fps))
+    paste0('fps=fps=', fps)
+  framerate <- av_video_info(video)$video$framerate
+  dir.create(destdir)
+  output <- file.path(destdir, 'image_%6d.jpg')
+  av_encode_video(input = video, output = output, framerate = framerate, vfilter = vfilter)
+  list.files(destdir, pattern = 'image_\\d{6}.jpg', full.names = TRUE)
+}
