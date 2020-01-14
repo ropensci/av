5d04e788e0e0e82e6d54e1df470fd40cf16a8778
Jeroen Ooms <jeroenooms@gmail.com>
2020-01-14 12:42:29 +0100

    Match colors of spectrogram with ffmpeg

--- R/fft.R
+++ R/fft.R
@@ -34,5 +34,11 @@ av_audio_fft <- function(audio, window = hanning(2048), overlap = 0.75, sample_r
 
 #' @export
 plot.av_fft <- function(x, y, ...){
-  graphics::image(attr(x, 'time'), attr(x, 'frequency'), t(x), xlab = 'TIME', ylab = 'FREQUENCY (HZ)')
+  plot.new()
+  par(mar=c(5, 5, 3,3))
+  par(bg='black', col.axis='white', fg='white', family='mono', font=2, col.lab='white', col.main='white', mex=0.6)
+  #col <- rev(viridisLite::inferno(12))
+  col <- c("#FCFFA4FF", "#F5DB4BFF", "#FCAD12FF", "#F78311FF", "#E65D2FFF", "#CB4149FF",
+           "#A92E5EFF", "#85216BFF", "#60136EFF", "#3A0963FF", "#140B35FF", "#000004FF")
+  graphics::image(attr(x, 'time'), attr(x, 'frequency'), t(x), xlab = 'TIME', ylab = 'FREQUENCY (HZ)', col = col)
 }
