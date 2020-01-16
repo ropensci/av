e698f14fbe09ef64732c2aabd188ec291e9dc29b
Jeroen Ooms <jeroenooms@gmail.com>
2020-01-16 13:46:21 +0100

    Muffle test warnings

--- tests/testthat/test-fft.R
+++ tests/testthat/test-fft.R
@@ -3,6 +3,7 @@ context("Audio FFT")
 test_that("Audio FFT", {
   wonderland <- system.file('samples/Synapsis-Wonderland.mp3', package='av')
   extensions <- c("wav", "mkv", "ac3", "flac")
+  av_log_level(16) # muffle some warnings about ac3 vbr
   for(ext in extensions){
     filename <- paste0('wonderland.', ext)
     av_audio_convert(wonderland, filename, verbose = FALSE)
