c1fb6c7cae1a81468ba1a2f0a2383e220a600dd8
Jeroen Ooms <jeroenooms@gmail.com>
2020-09-28 12:33:22 +0200

    Workaround bug in ps for network connections See: https://github.com/r-lib/ps/issues/90

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,8 @@ library(testthat)
 library(av)
 
 if (ps::ps_is_supported()) {
-  reporter <- ps::CleanupReporter(testthat::CheckReporter)$new(proc_cleanup =  FALSE,  proc_fail = FALSE)
+  # This sometimes has false positives on MacOS https://github.com/r-lib/ps/issues/90
+  reporter <- ps::CleanupReporter(testthat::CheckReporter)$new(proc_cleanup = FALSE, proc_fail = FALSE, conn_fail = FALSE)
   test_check("av", reporter = reporter)
 } else {
   test_check("av")
