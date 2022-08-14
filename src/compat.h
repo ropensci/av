455fa3166216ee39e67ec860cfa6d324c92c23a1
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-14 11:40:47 +0200

    Future proof

--- src/compat.h
+++ src/compat.h
@@ -1,4 +1,4 @@
 /* This changed in ffmpeg 5.1, see https://github.com/FFmpeg/FFmpeg/commit/086a8048 */
-#if LIBAVUTIL_VERSION_MAJOR >= 57 && defined(FF_API_OLD_CHANNEL_LAYOUT)
+#if LIBAVUTIL_VERSION_MAJOR >= 58 || (LIBAVUTIL_VERSION_MAJOR >= 57 && defined(FF_API_OLD_CHANNEL_LAYOUT))
 #define NEW_CHANNEL_API
 #endif
