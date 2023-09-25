b69e58bbbc01c96cee9f40868b30bf78e1ccc643
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-25 19:11:41 +0200

    Fix clang x86

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,6 +2,8 @@ if(!file.exists("../windows/ffmpeg/include/libavfilter/avfilter.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
     "https://github.com/r-windows/bundles/releases/download/ffmpeg-6.0/ffmpeg-6.0-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/ffmpeg-6.0/ffmpeg-6.0-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
     "https://github.com/r-windows/bundles/releases/download/ffmpeg-6.0/ffmpeg-6.0-ucrt-x86_64.tar.xz"
   } else {
