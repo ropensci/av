cbf9c330baa9b5394a1354df7c25d502230581ef
Jeroen Ooms <jeroenooms@gmail.com>
2024-08-07 10:27:23 +0200

    Test without cleanupreporter

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(av)
 
-if (ps::ps_is_supported()) {
+if (0 && ps::ps_is_supported()) {
   # This sometimes has false positives on MacOS https://github.com/r-lib/ps/issues/90
   reporter <- ps::CleanupReporter(testthat::CheckReporter)$new(proc_cleanup = FALSE, proc_fail = FALSE, conn_fail = FALSE)
   test_check("av", reporter = reporter)
